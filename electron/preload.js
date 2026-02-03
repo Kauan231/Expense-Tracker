2b490b02a54e87d6fa35486b6bfacf7eb40d30d2
Kauan231 <kauanramos03@outlook.com>
2026-02-03 19:38:57 -0300

    Add native open file

--- electron/preload.js
+++ electron/preload.js
@@ -1,5 +1,6 @@
-const { contextBridge } = require("electron");
+const { contextBridge, shell } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
   platform: process.platform,
+  openFile: (path) => shell.openPath(path),
 });
